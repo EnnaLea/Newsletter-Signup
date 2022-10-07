d017ea4ac268baac46b6207f2d1d0addd958e81f
EnnaLea <49825624+leaaliman@users.noreply.github.com>
2022-10-07 13:18:15 +0200

    change1

--- app.js
+++ app.js
@@ -1,16 +1,36 @@
 const express = require("express");
-const request = require("request");
-const bodyParser = require("body-parser");
+const https = require("https");
+// const bodyParser = require("body-parser");
+
+
 
 const app = express();
 
-app.use(express.static("public"));
+app.use(express.static("Public"));
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true }));
+
+
 
 app.get("/", function (req, res){
     res.sendFile(__dirname + "/signup.html");
 });
 
 
+
+app.post("/", function(req, res){
+
+    var firstName = req.body.Firstname;
+    var lastName = req.body.Lastname;
+    var email = req.body.Email;
+
+    console.log(firstName, lastName, email);
+
+});
+
+
+
 app.listen(8889, function(){
     console.log("Server is running on port 8889");
-})
+});
